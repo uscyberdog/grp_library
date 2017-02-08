d8f14525a288c5e9f0d8702fd6d30d97fc580ca6
Ross Weidner <rossweidner@Rosss-MacBook-Pro.local>
2017-02-08 12:46:47 -0600

    Login page update, logout routeupdates

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   resources :users
 
 	# login form
-  get 'login', to: 'sessions#new'
+  get '/login', to: 'sessions#new'
 
   #check to see if we have a user, then set session[:user_id]
- 	post 'login',to: 'sessions#create'
+ 	post '/login',to: 'sessions#create'
 
-  delete 'logout', to: 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
